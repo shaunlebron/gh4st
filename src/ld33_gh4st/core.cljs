a0c4514e2495968e80775e2ac20c1d771f18a728
Shaun Williams <shaun.e.williams@gmail.com>
2015-08-22 12:07:38 -0500

    draw actors

--- src/ld33_gh4st/core.cljs
+++ src/ld33_gh4st/core.cljs
@@ -14,25 +14,57 @@
 
 (defonce app-state
   (atom {:board (empty-board 7 6)
-         :actors {:pacman [0 1]
-                  :blinky [0 2]
-                  :end [3 5]
+         :level 0
+         :actors {:pacman {:pos [0 1] :dir [1 0]}
+                  :blinky {:pos [0 2] :dir [0 1]}
+                  :fruit {:pos [3 5]}
                   }
          }))
 
+(def actor-order
+  [:blinky
+   :pinky
+   :inky
+   :clyde
+   :pacman
+   :fruit])
+
+(def dir->name
+  {[0 1]  "-d"
+   [0 -1] "-u"
+   [1 0]  "-r"
+   [-1 0] "-l"})
+
+(defn img-src
+  [name- dir]
+  (if (= name- :fruit)
+    (str "img/" (name name-) (:level @app-state) ".png")
+    (str "img/" (name name-) (dir->name dir) ".png")))
+
 (defn on-js-reload []
   nil)
+
+(defn actor
+  [actors drawpos]
+  (for [name- actor-order]
+    (let [{:keys [pos dir]} (get actors name-)]
+      (when (= drawpos pos)
+        [:img.sprite {:src (img-src name- dir)}])
+      )
+    )
+  )
 
 (defcomponent board
   [data owner]
   (render [_this]
     (html
       [:div.board
-       (for [[y row] (map-indexed vector data)]
+       (for [[y row] (map-indexed vector (:board data))]
          [:div.row
           (for [[x cell] (map-indexed vector row)]
             [:div
              {:class (str "cell " (if (= :floor cell) "floor" "wall"))}
+             (actor (:actors data) [x y])
              ]
             )]
          )
@@ -45,7 +77,7 @@
   [data owner]
   (render [_this]
     (html
-      (om/build board (:board data))
+      (om/build board (select-keys data [:board :actors]))
       )))
 
 (om/root
