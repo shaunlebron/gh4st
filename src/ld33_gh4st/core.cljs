ed70ae7d8dbf31691b91a5b9495365a706dbe27b
Shaun Williams <shaun.e.williams@gmail.com>
2015-08-22 11:17:59 -0500

    add sprites, start drawing board

--- src/ld33_gh4st/core.cljs
+++ src/ld33_gh4st/core.cljs
@@ -7,16 +7,46 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn empty-board
+  "create initial empty board"
+  [w h]
+  (vec (repeat h (vec (repeat w :floor)))))
+
+(defonce app-state
+  (atom {:board (empty-board 7 6)
+         :actors {:pacman [0 1]
+                  :blinky [0 2]
+                  :end [3 5]
+                  }
+         }))
 
 (defn on-js-reload []
   nil)
+
+(defcomponent board
+  [data owner]
+  (render [_this]
+    (html
+      [:div.board
+       (for [[y row] (map-indexed vector data)]
+         [:div.row
+          (for [[x cell] (map-indexed vector row)]
+            [:div
+             {:class (str "cell " (if (= :floor cell) "floor" "wall"))}
+             ]
+            )]
+         )
+       ]
+      )
+    )
+  )
 
 (defcomponent root
   [data owner]
   (render [_this]
     (html
-      [:div (:text data)])))
+      (om/build board (:board data))
+      )))
 
 (om/root
   root
