2449a68483e9ff92251c13ec097eaa9b0c170fa1
Shaun Williams <shaun.e.williams@gmail.com>
2015-08-23 18:15:32 -0500

    add structure for level1

--- src/gh4st/levels.cljs
+++ src/gh4st/levels.cljs
@@ -16,21 +16,162 @@
     [:wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall]]})
 
 (def level1
-  {:actors
-   {:pacman {:pos [7 2], :dir [1 0]},
-    :blinky {:pos [12 2], :dir [-1 0]},
-    :pinky {:pos nil, :dir [0 0]},
-    :inky {:pos nil, :dir [0 0]},
-    :clyde {:pos nil, :dir [0 0]},
-    :fruit {:pos [14 2]}},
-   :board
-   [[:wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall]
-    [:wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall]
-    [:wall :wall :wall :wall :wall :wall :floor :floor :floor :floor :floor :floor :floor :floor :floor :wall :wall :wall :wall :wall :wall]
-    [:wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall]
-    [:wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall]
-    [:wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall]
-    [:wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall :wall]]}
+ {
+ :actors
+ {:pacman {:pos [9 1], :dir [-1 0]},
+  :blinky {:pos [9 5], :dir [1 0]},
+  :pinky {:pos nil, :dir [0 0]},
+  :inky {:pos nil, :dir [0 0]},
+  :clyde {:pos nil, :dir [0 0]},
+  :fruit {:pos [11 1]}},
+ :board
+ [[:wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall]
+  [:wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall]
+  [:wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :floor
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :floor
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall]
+  [:wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall]
+  [:wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall]
+  [:wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :floor
+   :floor
+   :floor
+   :floor
+   :floor
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall]
+  [:wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall
+   :wall 
+   :wall 
+   :wall 
+   :wall 
+   :wall 
+   :wall 
+   :wall 
+   :wall 
+   :wall 
+   :wall 
+   :wall]]} 
   )
 
 (def levels
