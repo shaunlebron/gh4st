2dd867e5da8aa8e560f9a20ae06e235d2c284fcf
Shaun Williams <shaun.e.williams@gmail.com>
2015-08-24 12:43:25 -0500

    add publish script

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+deploy_dir=deploy
+
+if [ ! -d "$deploy_dir" ]; then
+  git clone git@github.com:shaunlebron/gh4st.git $deploy_dir
+fi
+
+# Commit and push
+pushd $deploy_dir
+git reset HEAD .
+git rm -rf *
+cp -r ../resources/public/ .
+git add -u
+git add .
+git commit -m "update deploy"
+git push origin master:gh-pages
+
